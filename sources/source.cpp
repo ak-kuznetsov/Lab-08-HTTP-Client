6409855c0ae03fb86f7d3c93ddd94ec30b843793
ak-kuznetsov <a.kuznetsov2701@mail.ru>
2021-03-29 23:44:05 +0300

    refactoring

--- sources/source.cpp
+++ sources/source.cpp
@@ -5,5 +5,5 @@ int main(int argc, char* argv[]) {
   Client client(argc, argv);
   return client.Run();
 }
-//./cmake-build-debug/tests  http://localhost:8080/v1/api/suggest
-//{\"input\":\"hel\"}
+
+//./cmake-build-debug/demo  http://localhost:8080/v1/api/suggest {\"input\":\"hel\"}
--- tests/test.cpp
+++ tests/test.cpp
@@ -4,3 +4,4 @@
 #include <gtest/gtest.h>
 
 TEST(Example, EmptyTest) { SUCCEED(); }
+
