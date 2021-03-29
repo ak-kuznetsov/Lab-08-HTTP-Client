dd9601cf8ccf00eea7934f971b0f5a8f225e6223
ak-kuznetsov <a.kuznetsov2701@mail.ru>
2021-03-30 00:11:38 +0300

    refactoring

--- sources/source.cpp
+++ sources/source.cpp
@@ -3,7 +3,7 @@
 #include <Client.hpp>
 int main(int argc, char* argv[]) {
   Client client(argc, argv);
-  return client.Run();
+  client.Run();
 }
 
 //./cmake-build-debug/demo  http://localhost:8080/v1/api/suggest
